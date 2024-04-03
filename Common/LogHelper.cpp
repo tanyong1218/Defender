b921f38a478451db4f1b2ee11259d51c914d3353
1745820699 <1745820699@qq.com>
2024-04-03 18:21:45 +0800

    优化日志帮助模块

--- Common/LogHelper.cpp
+++ Common/LogHelper.cpp
@@ -1,5 +1,5 @@
 #include "LogHelper.h"
-
+#include "spdlog/async.h"
 
 LogHelper::~LogHelper()
 {
@@ -12,10 +12,9 @@ LogHelper::LogHelper()
 	auto basicSink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("../logs/basicSink.txt");
 	basicSink->set_level(spdlog::level::debug);
 	basicSink->set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%^%5l%$]  %v");
-	sinkList.push_back(basicSink);
-	m_logger = std::make_shared<spdlog::logger>("both", begin(sinkList), end(sinkList));
-
-	//register it if you need to access it globally
+	sinkList.push_back(std::move(basicSink));
+	// Consider using spdlog::async_logger for thread-safe logging
+	m_logger = std::make_shared<spdlog::async_logger>("both", begin(sinkList), end(sinkList), spdlog::thread_pool(), spdlog::async_overflow_policy::block);
 	spdlog::register_logger(m_logger);
 	m_logger->set_level(spdlog::level::err);
 	m_logger->flush_on(spdlog::level::err);
--- Common/LogHelper.h
+++ Common/LogHelper.h
@@ -39,42 +39,42 @@ public:
     {
         auto msg = fmt::format(fmt, std::forward<Args>(args)...);
         spdlog::info(("{}"), msg);
-        GetLogger()->info(("{}"), msg);
+        GetLogger()->info(("{}"), std::move(msg));
     }
     template<typename... Args>
     inline void error(format_string_t<Args...> fmt, Args &&...args)
     {
         auto msg = fmt::format(fmt, std::forward<Args>(args)...);
         spdlog::error(("{}"), msg);
-        GetLogger()->error(("{}"), msg);
+        GetLogger()->error(("{}"), std::move(msg));
     }
     template<typename... Args>
     inline void debug(format_string_t<Args...> fmt, Args &&...args)
     {
         auto msg = fmt::format(fmt, std::forward<Args>(args)...);
         spdlog::debug(("{}"), (msg));
-        GetLogger()->debug(("{}"), (msg));
+        GetLogger()->debug(("{}"), std::move(msg));
     }
     template<typename... Args>
     inline void critical(format_string_t<Args...> fmt, Args &&...args)
     {
         auto msg = fmt::format(fmt, std::forward<Args>(args)...);
         spdlog::critical(("{}"), (msg));
-        GetLogger()->critical(("{}"), (msg));
+        GetLogger()->critical(("{}"), std::move(msg));
     }
     template<typename... Args>
     inline void trace(format_string_t<Args...> fmt, Args &&...args)
     {
         auto msg = fmt::format(fmt, std::forward<Args>(args)...);
         spdlog::trace(("{}"), (msg));
-        GetLogger()->trace(("{}"), (msg));
+        GetLogger()->trace(("{}"), std::move(msg));
     }
     template<typename... Args>
     inline void warn(format_string_t<Args...> fmt, Args &&...args)
     {
         auto msg = fmt::format(fmt, std::forward<Args>(args)...);
         spdlog::warn(("{}"), (msg));
-        GetLogger()->warn(("{}"), (msg));
+        GetLogger()->warn(("{}"), std::move(msg));
     }
 
 private:
